f4ad6798298296906aa9f75a28c226623b931d16
rjrethesh@gmail.com <c.pallavi2018@vitstudent.ac.in>
2020-10-31 19:18:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,154 @@
+'use strict';
+
+var data1 = require('./lib/plaidSandbox2.json');
+
+var purchase = require('./purchase.js');
+var fetchAlts = require('./fetchAlts.js');
+
+var envvar = require('envvar');
+var express = require('express');
+var bodyParser = require('body-parser');
+var moment = require('moment');
+var plaid = require('plaid');
+
+var transactions;
+
+var APP_PORT = envvar.number('APP_PORT', 8000);
+
+var debug = true;
+
+var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID', debug ? '5a21a2ac4e95b836d37e3672' : '5616c9f51abbf9e13f581fb2' );
+var PLAID_SECRET = envvar.string('PLAID_SECRET', debug ? 'c8dbac9121245f93510d139d270519' : '488760dbd0560fbb10d5845bf03a24');
+var PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY', debug ? 'b8ef71cf0f8b7bdfe6d8da3761a04a' : '505704614c705498646afea6bffe29');
+var PLAID_ENV = envvar.string('PLAID_ENV', debug ? 'sandbox' : 'production' );
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+var ACCESS_TOKEN = null;
+var PUBLIC_TOKEN = null;
+var ITEM_ID = null;
+
+var client = new plaid.Client(
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  plaid.environments[PLAID_ENV]
+);
+
+var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response, next) {
+  response.render('index.ejs', {
+    PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
+    PLAID_ENV: PLAID_ENV,
+  });
+});
+
+app.post('/get_access_token', function(request, response, next) {
+  console.log(request.body);
+  PUBLIC_TOKEN = request.body.public_token;
+  console.log(PUBLIC_TOKEN);
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error != null) {
+      var msg = 'Could not exchange public_token!';
+      console.log(msg + '\n' + JSON.stringify(error));
+      return response.json({
+        error: msg
+      });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    console.log('Access Token: ' + ACCESS_TOKEN);
+    console.log('Item ID: ' + ITEM_ID);
+    response.json({
+      'error': false
+    });
+  });
+});
+
+app.get('/accounts', function(request, response, next) {
+
+  client.getAuth(ACCESS_TOKEN, function(error, authResponse) {
+    if (error != null) {
+      var msg = 'Unable to pull accounts from the Plaid API.';
+      console.log(msg + '\n' + error);
+      return response.json({
+        error: msg
+      });
+    }
+    console.log(authResponse.accounts);
+    response.json({
+      error: false,
+      accounts: authResponse.accounts,
+      numbers: authResponse.numbers,
+    });
+  });
+});
+
+app.post('/item', function(request, response, next) {
+
+  client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
+    if (error != null) {
+      console.log(JSON.stringify(error));
+      return response.json({
+        error: error
+      });
+    }
+
+    client.getInstitutionById(itemResponse.item.institution_id, function(err, instRes) {
+      if (err != null) {
+        var msg = 'Unable to pull institution information from the Plaid API.';
+        console.log(msg + '\n' + error);
+        return response.json({
+          error: msg
+        });
+      } else {
+        response.json({
+          item: itemResponse.item,
+          institution: instRes.institution,
+        });
+      }
+    });
+  });
+});
+
+app.get('/transactions', function(request, response, next) {
+
+  //start and end dates for transaction
+  var startDate = moment().subtract(160, 'days').format('YYYY-MM-DD');
+  var endDate = moment().format('YYYY-MM-DD');
+  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    count: 250,
+    offset: 0,
+  }, function(error, transactionsResponse) {
+    if (error != null) {
+      console.log(JSON.stringify(error));
+      return response.json({
+        error: error
+      });
+    }
+
+    transactions = debug ? transactionsResponse.transactions.concat(data1.transactions) : transactionsResponse.transactions;
+
+    transactions = transactions.map(entry => purchase.purchase(entry.product_name==null ? entry.name : entry.product_name, entry.amount, entry.date, entry.category_id));
+
+    console.dir(transactions, {depth: null});
+
+    transactions = fetchAlts(transactions).then((output) => {
+
+      response.json(output.filter(n => !!n));
+    
+    });
+    
+  });
+});
+
+var server = app.listen(APP_PORT, function() {
+  console.log('plaid-walkthrough server listening on port ' + APP_PORT);
+});
---
+++ purchase.js
@@ -0,0 +1,142 @@
+var exports = module.exports = {};
+
+const keywords = require("./keywords.json");
+const words = keywords.words;
+const wordmap = keywords.map;
+const category_map = keywords.category_map;
+const queue_map = keywords.queue_map;
+const threshold_map = keywords.threshold_map;
+const debug = true;
+
+//first tag indiv data, then tag again for long-term trends
+exports.purchase = function(item, price, date, category){
+  let attributes = function(item, price, date, category){
+    let init = [];
+    for (let type of Object.keys(words)) {
+      for (let key of words[type]) {
+        if (~item.indexOf(key)) {
+          init.push(wordmap[type]);
+          break;
+        }
+      }
+    }
+    let reversed = Object.keys(category_map).reverse();
+    for(let id of reversed){
+      if(category != null && category.startsWith(id) && !init.includes(category_map[id])){
+        init.push(category_map[id]);
+        break;
+      }
+    }
+    return init;
+  };
+  return{
+    item: item,
+    price: price,
+    date: date,
+    category: category,
+    attributes: attributes(item, price, date, category)
+  }
+}
+
+exports.binnedPurchases = function(allPurchases){
+  function init(length){
+    init_array = [];
+    for(i = 0; i < length; i++){
+      init_array.push([]);
+    }
+    return init_array;
+  }
+
+  var init_array = init(Object.keys(queue_map).length);
+
+  for(let purchase of allPurchases){
+    for(let attr of purchase.attributes){
+      init_array[queue_map[attr]].push(purchase);
+    }
+  }
+  return init_array;
+}
+
+exports.get = function(queues, query){
+  //console.log(queues);
+  var all = [];
+  for(i = 0; i < queues.length; i++){
+    all = all.concat(queues[i]);
+  }
+  var sort_by = function(field, reverse, primer){
+    var key = primer ? 
+      function(x) {return primer(x[field])} : 
+      function(x) {return x[field]};
+
+    reverse = !reverse ? 1 : -1;
+
+    return function (a, b) {
+      return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+    } 
+  }
+  all.sort(sort_by('date', true, parseInt));
+  let all_no_dupe = all.filter((other, index, self) => self.findIndex(t => t.item === other.item && t.price === other.price && t.date === other.date && t.category === other.category) === index);
+  //console.log(all_no_dupe);
+  if(query === "price"){
+    var price = 0.00;
+    for(let purchase of all_no_dupe){
+      price += parseFloat(purchase.price);
+    }
+    return price;
+  }
+  else if(query === "all"){
+    return JSON.stringify(all_no_dupe);
+  }
+}
+
+exports.getRecurrent = function(queues, indices){
+  let isRecurrent = function(queue, label){
+    var sum = 0.0;
+    let currTime = Math.floor((new Date).getTime()/1000);
+    let model = function(epoch_time, start_time){
+      let days_elapsed = Math.floor((epoch_time - start_time)/(60*60*24));
+      if(debug){
+        //console.log(label);
+        //console.log("start_time " + new Date(start_time * 1000) + " epoch_time " + new Date(epoch_time * 1000));
+        //console.log(days_elapsed);
+      }
+      if(days_elapsed <= 30){
+        return 1;
+      }
+      return Math.pow(Math.E, -((days_elapsed - 30)/12));
+    }
+    for(let purchase of queue){
+      sum += model(currTime, purchase.date);
+    }
+    //console.log("ratio " + sum/queue.length);
+    return sum/queue.length > threshold_map[label];
+  }
+  var recurring = [];
+  for(let i of indices){
+    if(isRecurrent(queues[i],Object.keys(queue_map)[i])){
+      recurring = recurring.concat(queues[i]);
+    }
+  }
+  return JSON.stringify(recurring);
+}
+
+if(debug){
+  let p = exports.purchase('kfc', 30, '1506884160','13005032');
+  //tag(p);
+  //console.log(p.item);
+
+  let test_epoch_dates = [/*1512154560,*//*1511636160,*./*1510858560,*//*1510772160,*//*1509562560,*/1509216960,1509216960,1508612160,1505501760,1503255360];
+
+  function set(){
+    let init = [];
+    for(i = 0; i < 5; i++){
+      init.push(exports.purchase('steam', 4.00, test_epoch_dates[i], '19013001'));
+    }
+    return init;
+  }
+  let all = exports.binnedPurchases(set());
+  console.log(all);
+  console.log(Object.keys(queue_map));
+  console.log(exports.get(all, "price"));
+  console.log(exports.getRecurrent(all,[0,1,2,4,8])); //always call w/ these indices if you want to get the recurring stuff
+}
